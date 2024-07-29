4facd46bb42af5a90210a57cf425ecc20f918f65
joshhovis <joshuahovis3@gmail.com>
2024-07-29 11:35:22 -0400

    Update path for images and add styling for nav bar

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,11 @@
-// import styles from "./Card.module.css";
+import styles from "./Card.module.css";
 
 const Card = ({ item }) => (
     <div>
+        <img src={item.thumbnail.regular.small} alt={item.title + "preview"} />
+        <p>{item.year}</p>
+        <p>{item.category}</p>
+        <p>{item.rating}</p>
         <h3>{item.title}</h3>
     </div>
 );
--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,54 @@
-import Link from "next/link";
 import styles from "./Header.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/images/logo.svg";
+import profilePic from "../public/images/image-avatar.png";
+import homeIcon from "../public/images/icon-nav-home.svg";
+import moviesIcon from "../public/images/icon-nav-movies.svg";
+import tvSeriesIcon from "../public/images/icon-nav-tv-series.svg";
+import bookmarkIcon from "../public/images/icon-nav-bookmark.svg";
 
 const Header = () => (
-    <header>
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/movies">Movies</Link>
-            <Link href="/tv-series">TV Series</Link>
-            <Link href="/bookmarked">Bookmarked</Link>
+    <header className={styles.header}>
+        <nav className={styles.nav}>
+            <Image className={styles.logo} src={logo} alt="Logo" />
+
+            <div className={styles.icons}>
+                <Link href="/">
+                    <Image
+                        className={styles.icon}
+                        src={homeIcon}
+                        alt="Home Icon"
+                    />
+                </Link>
+                <Link href="/movies">
+                    <Image
+                        className={styles.icon}
+                        src={moviesIcon}
+                        alt="Movies Icon"
+                    />
+                </Link>
+                <Link href="/tv-series">
+                    <Image
+                        className={styles.icon}
+                        src={tvSeriesIcon}
+                        alt="TV Series Icon"
+                    />
+                </Link>
+                <Link href="/bookmarked">
+                    <Image
+                        className={styles.icon}
+                        src={bookmarkIcon}
+                        alt="Bookmark Icon"
+                    />
+                </Link>
+            </div>
+
+            <Image
+                className={styles.user}
+                src={profilePic}
+                alt="User profile"
+            />
         </nav>
     </header>
 );
